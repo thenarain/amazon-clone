b4e62599c998a0528d3652be0820261aca7e361e
Kartikay <kartiksrivastava19@gmail.com>
2023-07-16 02:22:44 +0530

    Add HomeScreen and ProductScreen route in react using react-router Add new property countInStock f the product refactor the code style ProductScreen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,34 @@
-import data from "./data";
-import Product from "./components/Product";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 const App = () => {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazon
-          </a>
-        </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazon
+            </a>
+          </div>
 
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/sign-in">Sign-in</a>
-        </div>
-      </header>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/sign-in">Sign-in</a>
+          </div>
+        </header>
 
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
+        <main>
+          <Routes>
+            <Route path="/" Component={HomeScreen} exact />
+            <Route path="/product/:id" Component={ProductScreen} />
+          </Routes>
+        </main>
 
-      <footer className="row center">All right reserved</footer>
-    </div>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/screen/HomeScreen.js
@@ -0,0 +1,14 @@
+import data from "../data";
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  return (
+    <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+  );
+};
+
+export default HomeScreen;
